1b4e0cd774f66e1e5425b09406ecb6438fbc4a3f
Pablo Otoboni <pablootoboni@gmail.com>
2026-02-26 20:47:28 -0300

    Adiocionado logíca para item.js

--- 06-shopee-cart/src/index.js
+++ 06-shopee-cart/src/index.js
@@ -1,3 +1,11 @@
+import createItem from "./services/item.js";
+
 const cart = []
 
-console.log("Welcome to your cart");
+console.log("Welcome to the your cart");
+
+const item1 = await createItem("Hotwheels ferrari", 20.99, 1)
+const item2 = await createItem("Hotwheels lamborhuini", 39.99, 3)
+
+console.log(item2.subtotal());
+
