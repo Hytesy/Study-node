055c1334b93d5f9afa1d5833a5a6c8c31dfdfdf0
Pablo Otoboni <pablootoboni@gmail.com>
2026-05-17 11:36:48 -0300

    Adicionando sistema de escolha

--- 10-Qrcode/src/index.js
+++ 10-Qrcode/src/index.js
@@ -1,7 +1,12 @@
 import prompt from 'prompt'
+import { mainPrompt } from "./prompts/prompt-main.js"
 
 async function main() {
+    prompt.get(mainPrompt, async (err, choose) => {
+        if(choose.select == 1) console.log("Escolheu o QRcode")
+        if(choose.select == 2) console.log("Escolheu o password")
+    }) 
 
+    prompt.start()
 }
-
-main()
+main();
--- 10-Qrcode/src/prompts/prompt-main.js
+++ 10-Qrcode/src/prompts/prompt-main.js
@@ -1,4 +1,4 @@
-const mainPrompt = [
+export const mainPrompt = [
     {
         name: "select",
         description: "Escolha a ferramente (1 - QRCODE ou 2- PASSWORD)",
